14ac361bdfad641a320c614201cfb57bc47a6c12
Fred <thefredjacobs@gmail.com>
2022-11-25 13:22:31 +0200

    2022-11-25

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar>
+    <template v-slot:append>
+      <v-btn icon="mdi-heart"></v-btn>
+
+      <v-btn icon="mdi-magnify"></v-btn>
+
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-toolbar-title>WP HELP</v-toolbar-title>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" temporary>
+    <!--  -->
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let drawer = ref(null);
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app id="inspire">
+    <!--  <v-system-bar>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar> -->
+
+    <header-component />
+
+    <v-main class="bg-grey-lighten-2">
+      <slot />
+    </v-main>
+
+    <footer-component />
+  </v-app>
+</template>
+
+<script setup></script>
